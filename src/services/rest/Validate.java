d98f08166de7590983cc62285f1b0e91b744a78b
John Deck <jdeck88@gmail.com>
2016-02-11 15:13:43 -0800

    changing triplify.run() method call to send file to that service

--- src/services/rest/Validate.java
+++ src/services/rest/Validate.java
@@ -229,7 +229,7 @@ public class Validate extends FimsService {
         String outputPrefix = processController.getExpeditionCode() + "_output";
         Triplifier triplifier = new Triplifier(outputPrefix, uploadPath(), processController);
 
-        triplifier.run();
+        triplifier.run(processController.getValidation().getSqliteFile());
 
         // upload the dataset
         Uploader uploader = new Uploader(processController.getValidation().getMapping().getMetadata().getTarget(),
