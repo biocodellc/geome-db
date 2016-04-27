3b6c69c234ce4ac6ddc24c4c9d9200990f228781
rjewing <ewing.rj@gmail.com>
2016-04-27 07:30:58 -0700

    added a loading modal to the ExpeditionManager page

--- src/main/web/app/components/expeditions/expeditionController.js
+++ src/main/web/app/components/expeditions/expeditionController.js
@@ -1,13 +1,14 @@
-angular.module('fims.expeditions', ['fims.users'])
+angular.module('fims.expeditions', ['fims.users', 'fims.modals'])
 
-.controller('ExpeditionCtrl', ['$http', 'UserFactory', '$scope',
-    function ($http, UserFactory, $scope) {
+.controller('ExpeditionCtrl', ['$http', 'UserFactory', '$scope', 'LoadingModalFactory',
+    function ($http, UserFactory, $scope, LoadingModalFactory) {
         var vm = this;
         vm.totalItems;
         vm.itemsPerPage = 100;
         vm.currentPage = 1;
         vm.pageChanged = pageChanged;
         vm.content = [];
+        vm.initialized = false;
         fetchPage();
 
         function pageChanged() {
@@ -15,18 +16,16 @@ angular.module('fims.expeditions', ['fims.users'])
         }
 
         function fetchPage() {
-            var deferred = $.Deferred();
-            loadingDialog(deferred.promise());
+            LoadingModalFactory.open();
 
             $http.get('/biocode-fims/rest/projects/25/expeditions/datasets/latest?page=' + vm.currentPage + "&limit=" + vm.itemsPerPage)
                 .then(function(response) {
                     angular.extend(vm.content, response.data.content);
                     vm.content = response.data.content;
                     vm.totalItems = response.data.totalElements;
-                    deferred.resolve();
-                }, function() {
-                    deferred.reject();
-                })
+                    LoadingModalFactory.modalInstance.close();
+                    vm.initialized = true;
+                });
         }
 
-    }]);
+    }])
