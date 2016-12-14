1893fc99c8dde75e3c4e66e7a092f83a7d2f1972
rjewing <ewing.rj@gmail.com>
2016-12-14 06:49:41 -0800

    fix projectSelect directive to keep existing selected project when including/excluding public projects

--- src/main/web/app/directives/projectSelect.directive.js
+++ src/main/web/app/directives/projectSelect.directive.js
@@ -1,74 +1,78 @@
 angular.module('biscicolApp')
 
-.directive('projectSelect', ['$rootScope', function($rootScope) {
-    var directive = {
-        restrict: 'EA',
-        templateUrl: 'app/directives/projectSelect.directive.html',
-        controller: 'ProjectSelectCtrl',
-        controllerAs: 'projectSelectVm',
-        scope: {},
-        bindToController: {
-            projectId: "=",
-            selectClasses: '@',
-            publicBtnClasses: '@'
-        },
-        link: function($scope, $element, $attributes) {
-            $rootScope.$broadcast('projectSelectLoadedEvent');
-        }
-    };
-    return directive;
-}])
-
-.controller('ProjectSelectCtrl', ['$scope', '$location', '$timeout', 'ProjectFactory', 'AuthFactory',
-    function($scope, $location, $timeout, ProjectFactory, AuthFactory) {
-        var vm = this;
-        vm.isAuthenticated = AuthFactory.isAuthenticated;
-        vm.includePublic = !AuthFactory.isAuthenticated;
-        vm.projects = null;
-        vm.projectId = null;
-        vm.getProjects = getProjects;
-        vm.setProject = setProject;
-
-        function getProjects() {
-            ProjectFactory.getProjects(vm.includePublic)
-                .then(getProjectsComplete);
-
-            function getProjectsComplete(response) {
-                vm.projects = response.data;
-                // angular.extend(vm.projects, response.data);
+    .directive('projectSelect', ['$rootScope', function ($rootScope) {
+        var directive = {
+            restrict: 'EA',
+            templateUrl: 'app/directives/projectSelect.directive.html',
+            controller: 'ProjectSelectCtrl',
+            controllerAs: 'projectSelectVm',
+            scope: {},
+            bindToController: {
+                projectId: "=?",
+                selectClasses: '@',
+                publicBtnClasses: '@'
+            },
+            link: function ($scope, $element, $attributes) {
+                $rootScope.$broadcast('projectSelectLoadedEvent');
             }
-        }
+        };
+        return directive;
+    }])
 
-        function setProject(projectId) {
-            if (vm.projects.length > 0) {
-                if (projectId) {
-                    vm.projectId = projectId;
-                } else if (!vm.projectId) {
-                    vm.projectId = ($location.search()['projectId']) ? $location.search()['projectId'] : null;
-                } else if (vm.projects.length == 1) {
-                    vm.projectId = vm.projects[0].projectId;
+    .controller('ProjectSelectCtrl', ['$scope', '$location', '$timeout', 'ProjectFactory', 'AuthFactory',
+        function ($scope, $location, $timeout, ProjectFactory, AuthFactory) {
+            var vm = this;
+            vm.isAuthenticated = AuthFactory.isAuthenticated;
+            vm.includePublic = !AuthFactory.isAuthenticated;
+            vm.projects = null;
+            vm.selectedProject = null;
+            vm.projectId = null;
+            vm.getProjects = getProjects;
+            vm.setProject = setProject;
+
+            function getProjects() {
+                ProjectFactory.getProjects(vm.includePublic)
+                    .then(getProjectsComplete);
+
+                function getProjectsComplete(response) {
+                    vm.projects = response.data;
+                    setProject();
                 }
-                $scope.$apply();
             }
-        }
 
-        (function() {
-            getProjects();
-        }).call();
+            function setProject() {
+                if (!vm.selectedProject) {
+                    if ($location.search()['projectId']) {
 
-        $scope.$watch('projectSelectVm.projects', function(newValue, oldValue) {
-            if (!newValue || newValue === oldValue) return;
+                        var projectId = $location.search()['projectId'];
 
-            $timeout(function(projectId) {
-                setProject(projectId);
-            }, 0, true, vm.projectId);
-        });
+                        angular.forEach(vm.projects, function (project) {
 
-        $scope.$watch('projectSelectVm.projectId', function(value) {
-            if (value > 0) {
-                // hack for existing jquery code
-                $('#projects').val(value).trigger('change');
+                            if (project.projectId == projectId) {
+                                vm.selectedProject = project;
+                            }
+                        });
+                    } else if (vm.projects.length === 1) {
+                        vm.selectedProject = vm.projects[0];
+                    }
+                }
             }
-        });
-}])
+
+            (function () {
+                getProjects();
+            }).call();
+
+            $scope.$watch('projectSelectVm.selectedProject', function (value) {
+                if (value && value.projectId > 0) {
+                    vm.projectId = value.projectId;
+
+                    // hack for existing jquery code
+                    $timeout(function () {
+                        $('#projects').trigger('change');
+                    });
+                } else {
+                    vm.projectId = null;
+                }
+            });
+        }]);
 
