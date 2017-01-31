2c164736abcb5e2f433379e956fe1a77dc92a285
John Deck <jdeck88@gmail.com>
2017-01-30 18:47:17 -0800

    modifying dumper.py script in response to comments from vertnet team

--- scripts/dumper.py
+++ scripts/dumper.py
@@ -8,9 +8,11 @@ import os
 import getopt
 from pprint import pprint
 
+basisOfRecord = 'PreservedSpecimen'
 username = ''
 password = ''
 outputfile = ''
+rowNum = 1
 reload(sys)
 sys.setdefaultencoding('utf8')
 
@@ -39,11 +41,72 @@ def main(argv):
    	print usage
 	sys.exit(2)
 
+def createDynamicProperties(weight,length):
+    values =[]
+    retValue = ''
+
+    if weight is not None:
+        weight = weight.strip()
+    else: 
+        weight = ''
+    if length is not None:
+        length = length.strip()
+    else:
+        length = ''
+
+    if (weight != ''):
+        values.append('\'weightInGrams\':\''+weight+'\'')
+    if (length!= ''):
+        values.append('\'lengthInCentimeters\':\''+length+'\'')
+
+    if (len(values) == 0):
+        return ''
+    else:
+        retValue += '{'
+        count = 0
+        for s in values:
+            if (count):
+                retValue += ','
+            count += 1
+            retValue += values[0]
+        retValue += '}'
+
+        return retValue
+
+
+#Friendly method for creating dates
+def createDate(year,month,day):
+    if year is not None:
+        year = year.strip()
+    else:
+        year = ''
+    if month is not None:
+        month = month.strip()
+    else:
+        month = ''
+    if day is not None:
+        day = day.strip()
+    else:
+        day = '';
+
+    retValue = ''
+    if year == '':
+        return retValue
+    if month == '':
+        return year
+    if day == '':
+        return year +'-'+month
+    else: 
+        return year+'-'+month+'-'+day
+
+
 # Function for writing expeditionCode output
 def writeOutput(expeditionCode,csvfile,page,auth):
+    global basisOfRecord
+    global rowNum
     #if page == 0:
 #	# the list of fields in this header must match the list of fields in the list, below
-#    	header = [ 'materialSampleID','expeditionCode', 'associatedMedia', 'associatedReferences', 'associatedSequences', 'associatedTaxa', 'basisOfIdentification', 'class', 'coordinateUncertaintyInMeters', 'country', 'dayCollected', 'dayIdentified', 'decimalLatitude', 'decimalLongitude', 'establishmentMeans', 'eventRemarks', 'extractionID', 'family', 'fieldNotes', 'fundingSource', 'geneticTissueType', 'genus', 'georeferenceProtocol', 'habitat', 'identifiedBy', 'island', 'islandGroup', 'length', 'lifeStage', 'locality', 'maximumDepthInMeters', 'maximumDistanceAboveSurfaceInMeters', 'microHabitat', 'minimumDepthInMeters', 'minimumDistanceAboveSurfaceInMeters', 'monthCollected', 'monthIdentified', 'occurrenceID', 'occurrenceRemarks', 'order', 'permitInformation', 'phylum', 'plateID', 'preservative', 'previousIdentifications', 'previousTissueID', 'principalInvestigator', 'recordedBy', 'sampleOwnerInstitutionCode', 'samplingProtocol', 'sequence', 'sex', 'species', 'stateProvince', 'subSpecies', 'substratum', 'taxonRemarks', 'tissueStorageID', 'vernacularName', 'weight', 'wellID', 'wormsID', 'yearCollected', 'yearIdentified' ]
+#    	header = [ 'materialSampleID','dcterms_references','expeditionCode', 'associatedMedia', 'associatedReferences', 'associatedSequences', 'associatedTaxa', 'basisOfRecord', 'basisOfIdentification', 'class', 'coordinateUncertaintyInMeters', 'country', 'dateCollected', 'dateIdentified', 'decimalLatitude', 'decimalLongitude', 'dynamicProperties', 'establishmentMeans', 'eventRemarks', 'extractionID', 'family', 'fieldNotes', 'fundingSource', 'geneticTissueType', 'genus', 'georeferenceProtocol', 'habitat', 'identifiedBy', 'island', 'islandGroup', 'lifeStage', 'locality', 'maximumDepthInMeters', 'maximumDistanceAboveSurfaceInMeters', 'microHabitat', 'minimumDepthInMeters', 'minimumDistanceAboveSurfaceInMeters',  'occurrenceID', 'occurrenceRemarks', 'order', 'permitInformation', 'phylum', 'plateID', 'preservative', 'previousIdentifications', 'previousTissueID', 'principalInvestigator', 'recordedBy', 'sampleOwnerInstitutionCode', 'samplingProtocol', 'sequence', 'sex', 'species', 'stateProvince', 'subSpecies', 'substratum', 'taxonRemarks', 'tissueStorageID', 'vernacularName',  'wellID', 'wormsID'  ]
 #       csvfile.writerow(header)
     nextPage = page + 1
     url = "http://biscicol.org/dipnet/rest/v1.1/projects/query/json/?limit=10&page="+str(page)
@@ -53,6 +116,7 @@ def writeOutput(expeditionCode,csvfile,page,auth):
     # convert response to json
     text = response.text
     j = json.loads(text)
+    #import pdb;pdb.set_trace()
     totalPages = j['totalPages']
     #print str(page) + " of " + str(totalPages)
 
@@ -62,8 +126,9 @@ def writeOutput(expeditionCode,csvfile,page,auth):
     	for row in j['content']:
 	    # construct a list so we can use the csv file writer
 	    try:
-	    	list = [ row['materialSampleID'],expeditionCode, row['associatedMedia'], row['associatedReferences'], row['associatedSequences'], row['associatedTaxa'], row['basisOfIdentification'], row['class'], row['coordinateUncertaintyInMeters'], row['country'], row['dayCollected'], row['dayIdentified'], "%.6f" %float(row['decimalLatitude']), "%.6f" %float(row['decimalLongitude']), row['establishmentMeans'], row['eventRemarks'], row['extractionID'], row['family'], row['fieldNotes'], row['fundingSource'], row['geneticTissueType'], row['genus'], row['georeferenceProtocol'], row['habitat'], row['identifiedBy'], row['island'], row['islandGroup'], row['length'], row['lifeStage'], row['locality'], row['maximumDepthInMeters'], row['maximumDistanceAboveSurfaceInMeters'], row['microHabitat'], row['minimumDepthInMeters'], row['minimumDistanceAboveSurfaceInMeters'], row['monthCollected'], row['monthIdentified'], row['occurrenceID'], row['occurrenceRemarks'], row['order'], row['permitInformation'], row['phylum'], row['plateID'], row['preservative'], row['previousIdentifications'], row['previousTissueID'], row['principalInvestigator'], row['recordedBy'], row['sampleOwnerInstitutionCode'], row['samplingProtocol'], row['sex'], row['species'], row['stateProvince'], row['subSpecies'], row['substratum'], row['taxonRemarks'], row['tissueStorageID'], row['vernacularName'], row['weight'], row['wellID'], row['wormsID'], row['yearCollected'], row['yearIdentified'] ]	
+                list = [ rowNum, row['bcid'], expeditionCode, row['associatedMedia'], row['associatedReferences'], row['associatedSequences'], row['associatedTaxa'], row['basisOfIdentification'], basisOfRecord, row['class'], row['coordinateUncertaintyInMeters'], row['country'], createDate(row['yearCollected'],row['monthCollected'],row['dayCollected']),createDate(row['yearIdentified'],row['monthIdentified'],row['dayIdentified']), createDynamicProperties(row['weight'],row['length']),"%.6f" %float(row['decimalLatitude']), "%.6f" %float(row['decimalLongitude']), row['establishmentMeans'], row['eventRemarks'], row['extractionID'], row['family'], row['fieldNotes'], row['fundingSource'], row['geneticTissueType'], row['genus'], row['georeferenceProtocol'], row['habitat'], row['identifiedBy'], row['island'], row['islandGroup'], row['lifeStage'], row['locality'], row['maximumDepthInMeters'], row['maximumDistanceAboveSurfaceInMeters'], row['microHabitat'], row['minimumDepthInMeters'], row['minimumDistanceAboveSurfaceInMeters'], row['occurrenceID'], row['occurrenceRemarks'], row['order'], row['permitInformation'], row['phylum'], row['plateID'], row['preservative'], row['previousIdentifications'], row['previousTissueID'], row['principalInvestigator'], row['recordedBy'], row['sampleOwnerInstitutionCode'], row['samplingProtocol'], row['sex'], row['species'], row['stateProvince'], row['subSpecies'], row['substratum'], row['taxonRemarks'], row['tissueStorageID'], row['vernacularName'], row['wellID'], row['wormsID']  ]	
             	csvfile.writerow(list)
+                rowNum = rowNum + 1
 	    except ValueError:
 		print "\tskipping bad data in line"
 	# call writeOutput for the next page 
