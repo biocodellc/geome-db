4a48a30250330b9d4919b2b63de45cd3db78ce7f
rjewing <ewing.rj@gmail.com>
2017-05-04 14:01:42 -0700

    update bcidCreator to submit any resourceType

--- src/main/web/app/components/creator/creatorController.js
+++ src/main/web/app/components/creator/creatorController.js
@@ -3,8 +3,4 @@ angular.module('fims.creator')
 .controller('CreatorCtrl', ['UserFactory', function (UserFactory) {
     var vm = this;
     vm.user = UserFactory.user;
-
-    angular.element(document).ready(function () {
-        getResourceTypesMinusDataset("resourceTypesMinusDataset");
-    });
 }]);
