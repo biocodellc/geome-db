a21aaa96b131e9b6662a380d04714e462f807640
RJ Ewing <ewing.rj@protonmail.com>
2018-05-07 15:26:22 -0700

    generateSraFiles endpoint is now re-enabled
    closes #15

--- src/main/java/biocode/fims/geome/sra/GeomeBioSampleMapper.java
+++ src/main/java/biocode/fims/geome/sra/GeomeBioSampleMapper.java
@@ -1,207 +1,215 @@
-//package biocode.fims.geome.sra;
-//
-//import biocode.fims.exceptions.SraCode;
-//import biocode.fims.fastq.fileManagers.FastqFileManager;
-//import biocode.fims.fimsExceptions.FimsRuntimeException;
-//import biocode.fims.ncbi.sra.submission.BioSampleMapper;
-//import com.fasterxml.jackson.databind.JsonNode;
-//import com.fasterxml.jackson.databind.node.ArrayNode;
-//import com.fasterxml.jackson.databind.node.ObjectNode;
-//import org.apache.commons.lang.StringUtils;
-//
-//import java.util.ArrayList;
-//import java.util.Iterator;
-//import java.util.List;
-//
-///**
-// * Class that maps geome project attributes to sra BioSample attributes
-// */
-//public class GeomeBioSampleMapper implements BioSampleMapper {
-//    private static final String BLANK_ATTRIBUTE = "missing";
-//    private static final List<String> BIOSAMPLE_HEADERS = new ArrayList<String>() {{
-//        add("sample_name");
-//        add("sample_title");
-//        add("organism");
-//        add("collection_date");
-//        add("geo_loc_name");
-//        add("tissue");
-//        add("biomaterial_provider");
-//        add("collected_by");
-//        add("depth");
-//        add("dev_stage");
-//        add("identified_by");
-//        add("lat_lon");
-//        add("sex");
-//        add("breed");
-//        add("host");
-//        add("age");
-//        add("bcid");
-//    }};
-//
-//    private final Iterator<JsonNode> resourcesIt;
-//
-//    public GeomeBioSampleMapper(ArrayNode resources) {
-//
-//        if (resources == null || resources.size() == 0) {
-//            throw new FimsRuntimeException(SraCode.MISSING_DATASET, 400);
-//        }
-//
-//        this.resourcesIt = resources.iterator();
-//    }
-//
-//    @Override
-//    public boolean hasNextSample() {
-//        return resourcesIt.hasNext();
-//    }
-//
-//    @Override
-//    public List<String> getHeaderValues() {
-//        return BIOSAMPLE_HEADERS;
-//    }
-//
-//    @Override
-//    public List<String> getBioSampleAttributes() {
-//        ObjectNode sample = (ObjectNode) resourcesIt.next();
-//        List<String> bioSampleAttributes = new ArrayList<>();
-//
-//        if (sample.has(FastqFileManager.CONCEPT_ALIAS)) {
-//            String organism;
-//            String species = getTextField(sample, "species");
-//            String genus = getTextField(sample, "genus");
-//
-//            if (!StringUtils.isBlank(genus)) {
-//                organism = genus;
-//                if (!StringUtils.isBlank(species)) {
-//                    organism += " " + species;
-//                }
-//            } else {
-//                organism = getTextField(sample, "phylum");
-//            }
-//
-//            bioSampleAttributes.add(getTextField(sample, "materialSampleID"));
-//            bioSampleAttributes.add(sample.at("/" + FastqFileManager.CONCEPT_ALIAS + "/libraryStrategy").asText() + "_" + organism.replace(" ", "_"));
-//            bioSampleAttributes.add(organism);
-//            bioSampleAttributes.add(getCollectionDate(sample));
-//
-//            StringBuilder geoLocSb = new StringBuilder();
-//            geoLocSb.append(getTextField(sample, "country"));
-//            // must start with a country, otherwise sra validation fails
-//            if (!StringUtils.isBlank(getTextField(sample, "locality")) & !StringUtils.isBlank(geoLocSb.toString())) {
-//                geoLocSb.append(": ");
-//                geoLocSb.append(getTextField(sample, "locality"));
-//            }
-//            bioSampleAttributes.add(modifyBlankAttribute(geoLocSb.toString()));
-//
-//            bioSampleAttributes.add(modifyBlankAttribute(getTextField(sample, "geneticTissueType")));
-//            bioSampleAttributes.add(getTextField(sample, "principalInvestigator"));
-//            bioSampleAttributes.add(modifyBlankAttribute(getTextField(sample, "recordedBy")));
-//
-//            StringBuilder depthSb = new StringBuilder();
-//            if (!StringUtils.isBlank(getTextField(sample, "minimumDepthInMeters"))) {
-//                depthSb.append(getTextField(sample, "minimumDepthInMeters"));
-//
-//                if (!StringUtils.isBlank(getTextField(sample, "maximumDepthInMeters"))) {
-//                    depthSb.append(", ");
-//                    depthSb.append(getTextField(sample, "maximumDepthInMeters"));
-//                }
-//            }
-//            bioSampleAttributes.add(modifyBlankAttribute(depthSb.toString()));
-//
-//            bioSampleAttributes.add(modifyBlankAttribute(getTextField(sample, "lifeStage")));
-//            bioSampleAttributes.add(modifyBlankAttribute(getTextField(sample, "identifiedBy")));
-//
-//            bioSampleAttributes.add(modifyBlankAttribute(getLatLong(sample)));
-//
-//            bioSampleAttributes.add(modifyBlankAttribute(getTextField(sample, "sex")));
-//            bioSampleAttributes.add(BLANK_ATTRIBUTE);
-//            bioSampleAttributes.add(BLANK_ATTRIBUTE);
-//            bioSampleAttributes.add(BLANK_ATTRIBUTE);
-//            bioSampleAttributes.add(sample.get("bcid").asText());
-//        }
-//
-//        return bioSampleAttributes;
-//    }
-//
-//    private String modifyBlankAttribute(String attribute) {
-//        if (StringUtils.isBlank(attribute)) {
-//            return BLANK_ATTRIBUTE;
-//        }
-//
-//        return attribute;
-//    }
-//
-//    private String getCollectionDate(ObjectNode sample) {
-//        StringBuilder collectionDate = new StringBuilder();
-//
-//        collectionDate.append(getTextField(sample, "yearCollected"));
-//
-//        String monthCollected = getTextField(sample, "monthCollected");
-//        String dayCollected = getTextField(sample, "dayCollected");
-//
-//        if (!StringUtils.isBlank(monthCollected)) {
-//            collectionDate.append("-");
-//            collectionDate.append(monthCollected);
-//
-//            if (!StringUtils.isBlank(dayCollected)) {
-//                collectionDate.append("-");
-//                collectionDate.append(dayCollected);
-//            }
-//        }
-//
-//        return modifyBlankAttribute(collectionDate.toString());
-//    }
-//
-//    private String getTextField(ObjectNode sample, String field) {
-//        if (sample.hasNonNull(field)) {
-//            return sample.get(field).asText();
-//        }
-//        return "";
-//    }
-//
-//    /**
-//     * if both lat and long are present, return a string containing the abs(decimalDegree) + Compass Direction
-//     * <p>
-//     * ex.
-//     * <p>
-//     * lat = -8, long = 140 would return "8 S 140 W"
-//     *
-//     * @param sample
-//     * @return
-//     */
-//    private String getLatLong(ObjectNode sample) {
-//        StringBuilder latLongSb = new StringBuilder();
-//
-//        if (!StringUtils.isBlank(getTextField(sample, "decimalLatitude")) &&
-//                !StringUtils.isBlank(getTextField(sample, "decimalLongitude"))) {
-//
-//            String latText = getTextField(sample, "decimalLatitude");
-//            String lngText = getTextField(sample, "decimalLongitude");
-//            try {
-//                Double lat = Double.parseDouble(latText);
-//
-//                if (lat < 0) {
-//                    latLongSb.append(Math.abs(lat)).append(" S");
-//                } else {
-//                    latLongSb.append(lat).append(" N");
-//                }
-//
-//                latLongSb.append(" ");
-//
-//                Double lng = Double.parseDouble(lngText);
-//
-//                if (lng < 0) {
-//                    latLongSb.append(Math.abs(lng)).append(" W");
-//                } else {
-//                    latLongSb.append(lng).append(" E");
-//                }
-//            } catch (NumberFormatException e) {
-//                latLongSb = new StringBuilder()
-//                        .append(latText)
-//                        .append(" ")
-//                        .append(lngText);
-//            }
-//        }
-//
-//        return latLongSb.toString();
-//    }
-//}
+package biocode.fims.geome.sra;
+
+import biocode.fims.digester.Entity;
+import biocode.fims.exceptions.SraCode;
+import biocode.fims.fastq.FastqRecord;
+import biocode.fims.fimsExceptions.FimsRuntimeException;
+import biocode.fims.models.records.Record;
+import biocode.fims.ncbi.sra.submission.BioSampleMapper;
+import biocode.fims.query.QueryResult;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.*;
+
+/**
+ * Class that maps geome project attributes to sra BioSample attributes
+ */
+public class GeomeBioSampleMapper implements BioSampleMapper {
+    private static final String BLANK_ATTRIBUTE = "missing";
+    private static final List<String> BIOSAMPLE_HEADERS = new ArrayList<String>() {{
+        add("sample_name");
+        add("sample_title");
+        add("organism");
+        add("collection_date");
+        add("geo_loc_name");
+        add("tissue");
+        add("biomaterial_provider");
+        add("collected_by");
+        add("depth");
+        add("dev_stage");
+        add("identified_by");
+        add("lat_lon");
+        add("sex");
+        add("breed");
+        add("host");
+        add("age");
+        add("bcid");
+    }};
+
+    private final Iterator<Record> recordsIt;
+    private final QueryResult fastqResults;
+    private final QueryResult parentResults;
+    private final Entity parentEntity;
+    private final String parentUniqueKey;
+
+    public GeomeBioSampleMapper(QueryResult fastqResults, QueryResult parentResults) {
+        this.fastqResults = fastqResults;
+        this.parentResults = parentResults;
+
+        if (fastqResults.records().size() == 0) {
+            throw new FimsRuntimeException(SraCode.MISSING_DATASET, 400);
+        }
+
+        this.parentEntity = parentResults.entity();
+        this.parentUniqueKey = parentEntity.getUniqueKeyURI();
+
+        this.recordsIt = fastqResults.records().iterator();
+    }
+
+    @Override
+    public boolean hasNextSample() {
+        return recordsIt.hasNext();
+    }
+
+    @Override
+    public List<String> getHeaderValues() {
+        return BIOSAMPLE_HEADERS;
+    }
+
+    @Override
+    public List<String> getBioSampleAttributes() {
+        FastqRecord record = (FastqRecord) recordsIt.next();
+        List<String> bioSampleAttributes = new ArrayList<>();
+
+        Record parent = getParentForRecord(record);
+
+        String organism;
+        String species = parent.get("urn:species");
+        String genus = parent.get("urn:genus");
+
+        if (!genus.equals("")) {
+            organism = genus;
+            if (!species.equals("")) {
+                organism += " " + species;
+            }
+        } else {
+            organism = parent.get("urn:phylum");
+        }
+
+        bioSampleAttributes.add(parent.get("urn:materialSampleID"));
+        bioSampleAttributes.add(record.libraryStrategy() + "_" + organism.replace(" ", "_"));
+        bioSampleAttributes.add(organism);
+        bioSampleAttributes.add(getCollectionDate(parent));
+
+        StringBuilder geoLocSb = new StringBuilder();
+        geoLocSb.append(parent.get("urn:country"));
+        // must start with a country, otherwise sra validation fails
+        if (!StringUtils.isBlank(parent.get("urn:locality")) & !StringUtils.isBlank(geoLocSb.toString())) {
+            geoLocSb.append(": ");
+            geoLocSb.append(parent.get("urn:locality"));
+        }
+        bioSampleAttributes.add(modifyBlankAttribute(geoLocSb.toString()));
+
+        bioSampleAttributes.add(modifyBlankAttribute(parent.get("urn:geneticTissueType")));
+        bioSampleAttributes.add(parent.get("urn:principalInvestigator"));
+        bioSampleAttributes.add(modifyBlankAttribute(parent.get("urn:recordedBy")));
+
+        StringBuilder depthSb = new StringBuilder();
+        if (!StringUtils.isBlank(parent.get("urn:minimumDepthInMeters"))) {
+            depthSb.append(parent.get("urn:minimumDepthInMeters"));
+
+            if (!StringUtils.isBlank(parent.get("urn:maximumDepthInMeters"))) {
+                depthSb.append(", ");
+                depthSb.append(parent.get("urn:maximumDepthInMeters"));
+            }
+        }
+        bioSampleAttributes.add(modifyBlankAttribute(depthSb.toString()));
+
+        bioSampleAttributes.add(modifyBlankAttribute(parent.get("urn:lifeStage")));
+        bioSampleAttributes.add(modifyBlankAttribute(parent.get("urn:identifiedBy")));
+
+        bioSampleAttributes.add(modifyBlankAttribute(getLatLong(parent)));
+
+        bioSampleAttributes.add(modifyBlankAttribute(parent.get("urn:sex")));
+        bioSampleAttributes.add(BLANK_ATTRIBUTE);
+        bioSampleAttributes.add(BLANK_ATTRIBUTE);
+        bioSampleAttributes.add(BLANK_ATTRIBUTE);
+        bioSampleAttributes.add(this.fastqResults.rootIdentifier() + parent.get(parentUniqueKey));
+
+        return bioSampleAttributes;
+    }
+
+    private String modifyBlankAttribute(String attribute) {
+        if (StringUtils.isBlank(attribute)) {
+            return BLANK_ATTRIBUTE;
+        }
+
+        return attribute;
+    }
+
+    private String getCollectionDate(Record record) {
+        StringBuilder collectionDate = new StringBuilder();
+
+        collectionDate.append(record.get("urn:yearCollected"));
+
+        String monthCollected = record.get("urn:monthCollected");
+        String dayCollected = record.get("urn:dayCollected");
+
+        if (!StringUtils.isBlank(monthCollected)) {
+            collectionDate.append("-");
+            collectionDate.append(monthCollected);
+
+            if (!StringUtils.isBlank(dayCollected)) {
+                collectionDate.append("-");
+                collectionDate.append(dayCollected);
+            }
+        }
+
+        return modifyBlankAttribute(collectionDate.toString());
+    }
+
+    /**
+     * if both lat and long are present, return a string containing the abs(decimalDegree) + Compass Direction
+     * <p>
+     * ex.
+     * <p>
+     * lat = -8, long = 140 would return "8 S 140 W"
+     *
+     * @param parent
+     */
+    private String getLatLong(Record parent) {
+        StringBuilder latLongSb = new StringBuilder();
+
+        if (!StringUtils.isBlank(parent.get("decimalLatitude")) &&
+                !StringUtils.isBlank(parent.get("decimalLongitude"))) {
+
+            String latText = parent.get("decimalLatitude");
+            String lngText = parent.get("decimalLongitude");
+            try {
+                Double lat = Double.parseDouble(latText);
+
+                if (lat < 0) {
+                    latLongSb.append(Math.abs(lat)).append(" S");
+                } else {
+                    latLongSb.append(lat).append(" N");
+                }
+
+                latLongSb.append(" ");
+
+                Double lng = Double.parseDouble(lngText);
+
+                if (lng < 0) {
+                    latLongSb.append(Math.abs(lng)).append(" W");
+                } else {
+                    latLongSb.append(lng).append(" E");
+                }
+            } catch (NumberFormatException e) {
+                latLongSb = new StringBuilder()
+                        .append(latText)
+                        .append(" ")
+                        .append(lngText);
+            }
+        }
+
+        return latLongSb.toString();
+    }
+
+    private Record getParentForRecord(FastqRecord record) {
+        String parentId = record.get(parentUniqueKey);
+        return this.parentResults.records()
+                .stream()
+                .filter(r -> Objects.equals(r.get(parentUniqueKey), parentId))
+                .findFirst()
+                .orElse(null);
+    }
+}
--- src/main/java/biocode/fims/rest/services/ProjectController.java
+++ src/main/java/biocode/fims/rest/services/ProjectController.java
@@ -1,13 +1,46 @@
 package biocode.fims.rest.services;
 
 import biocode.fims.application.config.FimsProperties;
+import biocode.fims.authorizers.QueryAuthorizer;
+import biocode.fims.digester.Entity;
+import biocode.fims.digester.FastaEntity;
+import biocode.fims.digester.FastqEntity;
+import biocode.fims.fimsExceptions.BadRequestException;
+import biocode.fims.fimsExceptions.FimsRuntimeException;
+import biocode.fims.fimsExceptions.ForbiddenRequestException;
+import biocode.fims.fimsExceptions.errorCodes.GenericErrorCode;
+import biocode.fims.geome.sra.GeomeBioSampleMapper;
+import biocode.fims.geome.sra.GeomeSraMetadataMapper;
+import biocode.fims.models.Project;
+import biocode.fims.models.records.RecordSet;
+import biocode.fims.ncbi.sra.submission.BioSampleAttributesGenerator;
+import biocode.fims.ncbi.sra.submission.BioSampleMapper;
+import biocode.fims.ncbi.sra.submission.SraMetadataGenerator;
+import biocode.fims.ncbi.sra.submission.SraMetadataMapper;
+import biocode.fims.query.QueryResults;
+import biocode.fims.query.dsl.Query;
+import biocode.fims.repositories.RecordRepository;
+import biocode.fims.rest.responses.FileResponse;
 import biocode.fims.rest.services.BaseProjectsController;
+import biocode.fims.run.Dataset;
+import biocode.fims.run.DatasetBuilder;
 import biocode.fims.service.ExpeditionService;
 import biocode.fims.service.ProjectService;
+import biocode.fims.tools.FileCache;
+import biocode.fims.utils.FileUtils;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import javax.servlet.ServletContext;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
 
 /**
  * project API endpoints
@@ -18,46 +51,78 @@ import javax.ws.rs.*;
 @Path("projects")
 public class ProjectController extends BaseProjectsController {
 
+    @Context
+    private ServletContext context;
+    private final QueryAuthorizer queryAuthorizer;
+    private final RecordRepository recordRepository;
+    private final FileCache fileCache;
+
     @Autowired
     ProjectController(ExpeditionService expeditionService, FimsProperties props,
-                      ProjectService projectService) {
+                      ProjectService projectService, QueryAuthorizer queryAuthorizer, RecordRepository recordRepository, FileCache fileCache) {
         super(expeditionService, props, projectService);
+        this.queryAuthorizer = queryAuthorizer;
+        this.recordRepository = recordRepository;
+        this.fileCache = fileCache;
     }
 
-//    @GET
-//    @Path("/{projectId}/expeditions/{expeditionCode}/generateSraFiles")
-//    @Produces("application/zip")
-//    public Response generateSraFiles(@PathParam("projectId") int projectId,
-//                                     @PathParam("expeditionCode") String expeditionCode) {
-//
-//        File configFile = new ConfigurationFileFetcher(projectId, defaultOutputDirectory(), true).getOutputFile();
-//
-//        Mapping mapping = new Mapping();
-//        mapping.addMappingRules(configFile);
-//
-//        ProcessController processController = new ProcessController(projectId, expeditionCode);
-//        processController.setOutputFolder(defaultOutputDirectory());
-//        processController.setMapping(mapping);
-//        fimsMetadataFileManager.setProcessController(processController);
-//        ArrayNode dataset = fimsMetadataFileManager.getDataset();
-//
-//        SraMetadataMapper metadataMapper = new GeomeSraMetadataMapper(dataset);
-//        BioSampleMapper bioSampleMapper = new GeomeBioSampleMapper(dataset);
-//
-//        File bioSampleFile = BioSampleAttributesGenerator.generateFile(bioSampleMapper, defaultOutputDirectory());
-//        File sraMetadataFile = SraMetadataGenerator.generateFile(metadataMapper, defaultOutputDirectory());
-//
-//        Map<String, File> fileMap = new HashMap<>();
-//        fileMap.put("bioSample-attributes.tsv", bioSampleFile);
-//        fileMap.put("sra-metadata.tsv", sraMetadataFile);
-//        fileMap.put("sra-step-by-step-instructions.pdf", new File(context.getRealPath("docs/sra-step-by-step-instructions.pdf")));
-//
-//        Response.ResponseBuilder response = Response.ok(FileUtils.zip(fileMap, defaultOutputDirectory()), "application/zip");
-//        response.header("Content-Disposition",
-//                "attachment; filename=sra-files.zip");
-//
-//        return response.build();
-//    }
+    /**
+     * TODO find a more suitable place for this. & Re-write *Mappers to be more robust
+     * @param projectId
+     * @param expeditionCode
+     * @return
+     */
+    @GET
+    @Path("/{projectId}/expeditions/{expeditionCode}/generateSraFiles")
+    @Produces("application/json")
+    public FileResponse generateSraFiles(@PathParam("projectId") int projectId,
+                                         @PathParam("expeditionCode") String expeditionCode) {
+
+        Project project = projectService.getProject(projectId);
+
+        Entity e = project.getProjectConfig().entities()
+                .stream()
+                .filter(FastqEntity.class::isInstance)
+                .findFirst()
+                .orElseThrow((Supplier<RuntimeException>) () -> new BadRequestException("Could not find FastqEntity for provided project"));
+
+        Entity parentEntity = project.getProjectConfig().entity(e.getParentEntity());
+
+        String q = "_expeditions_:" +
+                expeditionCode +
+                " _select_:" +
+                parentEntity.getConceptAlias();
+
+        Query query = Query.factory(project, e.getConceptAlias(), q);
+
+        if (!queryAuthorizer.authorizedQuery(Collections.singletonList(projectId), new ArrayList<>(query.expeditions()), userContext.getUser())) {
+            throw new FimsRuntimeException(GenericErrorCode.UNAUTHORIZED, 403);
+        }
+
+        QueryResults queryResults = recordRepository.query(query);
+
+        if (queryResults.isEmpty()) return null;
+
+        RecordSet parentRecordSet = new RecordSet(parentEntity, queryResults.getResult(parentEntity.getConceptAlias()).records(), false);
+        RecordSet recordSet = new RecordSet(e, queryResults.getResult(e.getConceptAlias()).records(), false);
+        recordSet.setParent(parentRecordSet);
+
+        SraMetadataMapper metadataMapper = new GeomeSraMetadataMapper(queryResults.getResult(e.getConceptAlias()), queryResults.getResult(parentEntity.getConceptAlias()));
+        BioSampleMapper bioSampleMapper = new GeomeBioSampleMapper(queryResults.getResult(e.getConceptAlias()), queryResults.getResult(parentEntity.getConceptAlias()));
+
+        File bioSampleFile = BioSampleAttributesGenerator.generateFile(bioSampleMapper);
+        File sraMetadataFile = SraMetadataGenerator.generateFile(metadataMapper);
+
+        Map<String, File> fileMap = new HashMap<>();
+        fileMap.put("bioSample-attributes.tsv", bioSampleFile);
+        fileMap.put("sra-metadata.tsv", sraMetadataFile);
+        fileMap.put("sra-step-by-step-instructions.pdf", new File(context.getRealPath("docs/sra-step-by-step-instructions.pdf")));
+
+        File zip = FileUtils.zip(fileMap, defaultOutputDirectory());
+        String fileId = fileCache.cacheFileForUser(zip, userContext.getUser(), "sra-files.zip");
+
+        return new FileResponse(uriInfo.getBaseUriBuilder(), fileId);
+    }
 
 //    /**
 //     * Return a JSON representation of the expedition's that a user is a member of. Each expedition
