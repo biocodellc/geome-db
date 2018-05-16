7846a1f4917e07c37c64861b59defb38b701f52a
RJ Ewing <ewing.rj@protonmail.com>
2018-05-16 07:57:01 -0700

    cleanup circleci config

---
+++ scripts/checkoutChildProjects.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# script to checkout a specific branch for current project &
+# all child project if present. The GIT_BRANCH env variable is
+# used to specify the branch. If not found, we default to the
+# current branch of the parent project.
+# version.
+
+git fetch
+
+if [ ! -z "$GIT_BRANCH" ]; then
+    branch=$(echo $GIT_BRANCH)
+    git checkout $GIT_BRANCH
+else
+    branch=$(git rev-parse --abbrev-ref HEAD)
+fi
+
+git checkout src/main/web/apidocs
+git pull
+
+function checkout()
+{
+    cmd='/{print $3}'
+    cmd="/$1 $cmd"
+    local dir=$(cat ${scriptDir}/../gradle.properties | awk -F '[ ]+' "$cmd")
+
+    if [ ! -z "$dir" ]; then
+        cd "$dir"
+        git fetch
+        git checkout $branch
+        git pull
+    fi;
+}
+
+checkout "biocodeFimsCommonsDir"
+checkout "biocodeFimsSequencesDir"
+checkout "biocodeFimsPhotosDir"
