e3820d1489bd278bb1faf012f0209191af4f49ee
RJ Ewing <ewing.rj@protonmail.com>
2018-08-22 06:48:42 -0700

    cleanup GeomeWebAppConfig

--- src/main/java/biocode/fims/application/config/GeomeWebAppConfig.java
+++ src/main/java/biocode/fims/application/config/GeomeWebAppConfig.java
@@ -1,8 +1,5 @@
 package biocode.fims.application.config;
 
-import biocode.fims.authorizers.QueryAuthorizer;
-import biocode.fims.service.ExpeditionService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -13,17 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @Import({GeomeAppConfig.class, FimsWebAppConfig.class})
 public class GeomeWebAppConfig {
-
-    @Autowired
-    GeomeAppConfig geomeAppConfig;
-
-    @Autowired
-    ExpeditionService expeditionService;
-
-    @Bean
-    public QueryAuthorizer queryAuthorizer(FimsProperties fimsProperties) {
-        return new QueryAuthorizer(geomeAppConfig.projectService, expeditionService, fimsProperties);
-    }
 
 }
 
