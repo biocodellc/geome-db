600d387a1c03a32021d37fad1ac7b553b70c2c61
RJ Ewing <ewing.rj@protonmail.com>
2018-10-04 11:28:18 -0700

    fix dev deployment script:

--- scripts/development_deploy.sh
+++ scripts/development_deploy.sh
@@ -6,11 +6,9 @@ git fetch
 git checkout develop
 git pull
 
-./scripts/generateDevelopRestApiDocs.sh
-
 ./gradlew clean &&
 ./scripts/generateDevelopRestApiDocs.sh
 ./gradlew -Penvironment=development fatWar &&
 
-sudo cp /home/deploy/code/prod/geome-db/dist/geome-db-fat.war /opt/web/prod/webapps/geome-db.war
-sudo /bin/touch /opt/web/prod/webapps/geome-db.xml
+sudo cp /home/deploy/code/dev/geome-db/dist/geome-db-fat.war /opt/web/dev/webapps/geome-db.war
+sudo /bin/touch /opt/web/dev/webapps/geome-db.xml
