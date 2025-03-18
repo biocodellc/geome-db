ff3e6374641c87e3a921be3ca0392f9ee8bfcdb8
John Deck <jdeck88@gmail.com>
2025-03-18 16:19:18 -0700

    updating logging in biosampleattributesgenerator.java

--- src/main/java/biocode/fims/ncbi/sra/submission/BioSampleAttributesGenerator.java
+++ src/main/java/biocode/fims/ncbi/sra/submission/BioSampleAttributesGenerator.java
@@ -9,7 +9,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /**
  * class to generate a NCBI sra BioSample attributes file (https://www.ncbi.nlm.nih.gov/biosample/docs/submission/faq/)
  * This takes the sample metadata stored in the FIMS system and writes it to a tsv file. The attributes file is compatible
@@ -24,40 +25,59 @@ public class BioSampleAttributesGenerator {
      * @param mapper {@link BioSampleMapper} implementation
      * @return
      */
+
+
     public static File generateFile(BioSampleMapper mapper) {
+        Logger logger = LoggerFactory.getLogger(BioSampleAttributesGenerator.class);
+
         File attributesFile = FileUtils.createUniqueFile("bioSample-attributes.tsv", System.getProperty("java.io.tmpdir"));
+        logger.info("Creating file: {}", attributesFile.getAbsolutePath());
 
         try (FileWriter fw = new FileWriter(attributesFile)) {
+            if (mapper == null) {
+                logger.error("BioSampleMapper is null");
+                throw new FimsRuntimeException(SraCode.METADATA_FILE_CREATION_FAILED, 500);
+            }
 
             List<String> headers = mapper.getHeaderValues();
-
-            Iterator<String> it = headers.iterator();
-            while (it.hasNext()) {
-                fw.write(it.next());
-                if (it.hasNext()) fw.write(DELIMITER);
+            if (headers == null || headers.isEmpty()) {
+                logger.warn("Header values are null or empty");
+            } else {
+                logger.info("Writing {} headers to file", headers.size());
+                Iterator<String> it = headers.iterator();
+                while (it.hasNext()) {
+                    fw.write(it.next());
+                    if (it.hasNext()) fw.write(DELIMITER);
+                }
+                fw.write("\n");
             }
-            fw.write("\n");
 
+            int sampleCount = 0;
             while (mapper.hasNextSample()) {
                 List<String> bioSampleAttributes = mapper.getBioSampleAttributes();
 
-                if (!bioSampleAttributes.isEmpty()) {
-                    it = bioSampleAttributes.iterator();
+                if (bioSampleAttributes == null) {
+                    logger.error("BioSample attributes list is null at sample index {}", sampleCount);
+                    continue;
+                }
 
+                if (!bioSampleAttributes.isEmpty()) {
+                    sampleCount++;
+                    Iterator<String> it = bioSampleAttributes.iterator();
                     while (it.hasNext()) {
                         fw.write(it.next());
                         if (it.hasNext()) fw.write(DELIMITER);
                     }
-
                     fw.write("\n");
                 }
             }
+            logger.info("Successfully wrote {} samples to file", sampleCount);
         } catch (IOException e) {
+            logger.error("IOException occurred while writing BioSample attributes file", e);
             throw new FimsRuntimeException(SraCode.METADATA_FILE_CREATION_FAILED, 500);
         }
 
         return attributesFile;
-
-
     }
+
 }
