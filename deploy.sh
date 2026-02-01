4293e8803c734407d522a4110b31a786ce5ea445
John Deck <jdeck88@gmail.com>
2026-01-31 16:16:48 -0800

    updating .gitignore

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+# instructions for development and deployment which is done on the command-line
+# first make sure we add localhost referrer in:
+# https://console.cloud.google.com/apis/credentials?project=berkeleymapper
+# add the following:
+# http://localhost:8080/*
+# make sure you disable this when done developing
+
+# build the war file
+./gradlew war
+# copy it into place on this machine
+cp build/libs/geome-db.war /usr/local/Cellar/jetty/9.4.50.v20221201/libexec/webapps/geome-db.war
+# open a test page
+#/usr/bin/open -a "/Applications/Google Chrome.app" 'http://localhost:8080/index.html?tabfile=https://raw.githubusercontent.com/BNHM/berkeleymapper/master/examples/awtest.txt&configfile=https://raw.githubusercontent.com/BNHM/berkeleymapper/master/examples/awtest.xml'
